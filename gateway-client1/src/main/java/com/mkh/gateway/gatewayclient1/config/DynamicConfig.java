ccccb5009747bb5e4369ddd9e01e806bd6c48257
문광훈 <mkh9293@gmail.com>
2019-06-05 09:37:54 +0900

    Update gateway-client
    2019-06-05
    config client 호출 부분 방식 변경 및 테스트

--- gateway-client1/src/main/java/com/mkh/gateway/gatewayclient1/config/DynamicConfig.java
+++ gateway-client1/src/main/java/com/mkh/gateway/gatewayclient1/config/DynamicConfig.java
@@ -1,11 +1,10 @@
 package com.mkh.gateway.gatewayclient1.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
-@RefreshScope
+//@RefreshScope
 public class DynamicConfig {
     @Value("${application.service.name}")
     private String name;
