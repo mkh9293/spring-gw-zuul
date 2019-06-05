b4a7ee8bf2373890ae2b3735c91bad4eeb85b098
문광훈 <mkh9293@gmail.com>
2019-06-05 10:47:54 +0900

    Update config file
    config test

--- gateway-client1/src/main/java/com/mkh/gateway/gatewayclient1/config/DynamicConfig.java
+++ gateway-client1/src/main/java/com/mkh/gateway/gatewayclient1/config/DynamicConfig.java
@@ -1,10 +1,11 @@
 package com.mkh.gateway.gatewayclient1.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
-//@RefreshScope
+@RefreshScope
 public class DynamicConfig {
     @Value("${application.service.name}")
     private String name;
